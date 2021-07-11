8721ebb60f3bab13e30b36735a4a3e226116976e
Mark Skilbeck <mark.skilbeck@rigetti.com>
2021-07-11 20:54:03 +0100

    Update: move VM and split out errors

---
+++ src/vm/errors.rs
@@ -0,0 +1,60 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum VMError {
+    /// Represents a failure due to providing an operand whose type is incompatible with the instruction
+    #[error("Invalid type for instruction operand {name}: expected {expected} but found {actual}")]
+    InvalidType {
+        name: String,
+        expected: String,
+        actual: String,
+    },
+
+    /// Represents a failure due to a mismatch between the expected type of a memory region and its actual type
+    #[error("Unexpected type for memory region: expected {expected} but found {actual}")]
+    UnexpectedMemoryRegionType { expected: String, actual: String },
+
+    /// Represents a failure due to a mismatch between the expected type of a literal value and its actual type
+    #[error("Unexpected literal type: expected {expected} but found {actual}")]
+    UnexpectedLiteralType { expected: String, actual: String },
+
+    /// Represents a failure due to referencing an undefined memory region
+    #[error("Undefined memory region {name:?}")]
+    UndefinedMemoryRegion { name: String },
+
+    /// Represents a failure due to using a qubit variable where a qubit index was expected
+    #[error("Unresolved qubit variable {name:?}")]
+    UnresolvedQubitVariable { name: String },
+
+    /// Represents a failure due to JUMPing to an undefined target
+    #[error("Undefined JUMP target {name:?}")]
+    UndefinedJumpTarget { name: String },
+
+    /// Represents a failure due to finding a duplicate LABEL
+    #[error("Duplicate LABEL {name:?}")]
+    DuplicateLabel { name: String },
+
+    #[error("Invalid destination for MOVE: expected a memory region but found {actual}")]
+    InvalidMoveDestination { actual: String },
+
+    #[error("Incompatible destination for EXCHANGE: due to source type, expected {right} but found {left}")]
+    IncompatibleExchange { left: String, right: String },
+
+    #[error("Invalid EXCHANGE: operands must be memory regions but found {left} and {right}")]
+    InvalidExchange { left: String, right: String },
+
+    #[error("Incompatible destination for MOVE: due to source type, expected {ssource} but found {destination}")]
+    IncompatibleLoad {
+        ssource: String,
+        destination: String,
+    },
+
+    #[error("Incompatible destination for STORE: due to source type, expected {ssource} but found {destination}")]
+    IncompatibleStore {
+        ssource: String,
+        destination: String,
+    },
+}
+
+// TODO VMError is nice but it doesn't show which instruction caused the error. Define a VMRunError
+// which wraps a VMError and includes the erroneous instruction.
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,7 +1,5 @@
 use std::{collections::HashMap, fmt, usize};
 
-use thiserror::Error;
-
 use quil::instruction::ArithmeticOperand;
 use quil::{
     instruction::{Instruction, MemoryReference, Qubit, ScalarType},
@@ -10,64 +8,8 @@ use quil::{
 
 use crate::{gates::QGate, matrix::instruction_matrix, wavefunction::Wavefunction};
 
-#[derive(Error, Debug)]
-pub enum VMError {
-    /// Represents a failure due to providing an operand whose type is incompatible with the instruction
-    #[error("Invalid type for instruction operand {name}: expected {expected} but found {actual}")]
-    InvalidType {
-        name: String,
-        expected: String,
-        actual: String,
-    },
-
-    /// Represents a failure due to a mismatch between the expected type of a memory region and its actual type
-    #[error("Unexpected type for memory region: expected {expected} but found {actual}")]
-    UnexpectedMemoryRegionType { expected: String, actual: String },
-
-    /// Represents a failure due to a mismatch between the expected type of a literal value and its actual type
-    #[error("Unexpected literal type: expected {expected} but found {actual}")]
-    UnexpectedLiteralType { expected: String, actual: String },
-
-    /// Represents a failure due to referencing an undefined memory region
-    #[error("Undefined memory region {name:?}")]
-    UndefinedMemoryRegion { name: String },
-
-    /// Represents a failure due to using a qubit variable where a qubit index was expected
-    #[error("Unresolved qubit variable {name:?}")]
-    UnresolvedQubitVariable { name: String },
-
-    /// Represents a failure due to JUMPing to an undefined target
-    #[error("Undefined JUMP target {name:?}")]
-    UndefinedJumpTarget { name: String },
-
-    /// Represents a failure due to finding a duplicate LABEL
-    #[error("Duplicate LABEL {name:?}")]
-    DuplicateLabel { name: String },
-
-    #[error("Invalid destination for MOVE: expected a memory region but found {actual}")]
-    InvalidMoveDestination { actual: String },
-
-    #[error("Incompatible destination for EXCHANGE: due to source type, expected {right} but found {left}")]
-    IncompatibleExchange { left: String, right: String },
-
-    #[error("Invalid EXCHANGE: operands must be memory regions but found {left} and {right}")]
-    InvalidExchange { left: String, right: String },
-
-    #[error("Incompatible destination for MOVE: due to source type, expected {ssource} but found {destination}")]
-    IncompatibleLoad {
-        ssource: String,
-        destination: String,
-    },
-
-    #[error("Incompatible destination for STORE: due to source type, expected {ssource} but found {destination}")]
-    IncompatibleStore {
-        ssource: String,
-        destination: String,
-    },
-}
-
-// TODO VMError is nice but it doesn't show which instruction caused the error. Define a VMRunError
-// which wraps a VMError and includes the erroneous instruction.
+pub mod errors;
+pub use errors::*;
 
 #[derive(Debug, Clone)]
 pub enum MemoryContainer {
@@ -100,12 +42,10 @@ impl MemoryContainer {
     pub fn bit(self) -> Result<Vec<i64>, VMError> {
         match self {
             MemoryContainer::Bit(v) | MemoryContainer::Integer(v) => Ok(v),
-            MemoryContainer::Real(_) => {
-                return Err(VMError::UnexpectedMemoryRegionType {
-                    expected: "BIT".to_string(),
-                    actual: self.type_of(),
-                })
-            }
+            MemoryContainer::Real(_) => Err(VMError::UnexpectedMemoryRegionType {
+                expected: "BIT".to_string(),
+                actual: self.type_of(),
+            }),
         }
     }
 
@@ -113,24 +53,20 @@ impl MemoryContainer {
         match self {
             MemoryContainer::Bit(b) => Ok(b),
             MemoryContainer::Integer(i) => Ok(i),
-            MemoryContainer::Real(_) => {
-                return Err(VMError::UnexpectedMemoryRegionType {
-                    expected: "INTEGER".to_string(),
-                    actual: self.type_of(),
-                })
-            }
+            MemoryContainer::Real(_) => Err(VMError::UnexpectedMemoryRegionType {
+                expected: "INTEGER".to_string(),
+                actual: self.type_of(),
+            }),
         }
     }
 
     pub fn real(self) -> Result<Vec<f64>, VMError> {
         match self {
             MemoryContainer::Real(r) => Ok(r),
-            _ => {
-                return Err(VMError::UnexpectedMemoryRegionType {
-                    expected: "REAL".to_string(),
-                    actual: self.type_of(),
-                })
-            }
+            _ => Err(VMError::UnexpectedMemoryRegionType {
+                expected: "REAL".to_string(),
+                actual: self.type_of(),
+            }),
         }
     }
 }
@@ -312,7 +248,7 @@ impl VM {
             }
 
             Instruction::Label(label) => {
-                if let Some(_) = self.labels.insert(label.clone(), self.pc) {
+                if self.labels.insert(label.clone(), self.pc).is_some() {
                     return Err(VMError::DuplicateLabel {
                         name: label.clone(),
                     });
@@ -708,6 +644,7 @@ impl fmt::Debug for VM {
     }
 }
 
+#[allow(unused_must_use)]
 #[cfg(test)]
 mod test {
     use std::str::FromStr;
